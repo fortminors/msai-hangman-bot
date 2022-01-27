91ec5ef3c39c4f32ec98471ade296fe294a71635
Georgy Gunkin <FortMinors@yandex.ru>
2022-01-28 01:52:59 +0300

    feat: added handling multiple players at the same time

--- bot.py
+++ bot.py
@@ -5,7 +5,9 @@ import random
 from typing import List
 
 from enum import Enum
+from dataclasses import dataclass
 
+from telebot.types import Message
 
 class AnswerType(Enum):
 	No = 0,
@@ -16,6 +18,46 @@ class GuessType(Enum):
 	Letter = 0,
 	Word = 1,
 	Error = 2,
+
+
+class Word():
+	def __init__(self, word):
+		self.q = "\U00002753"
+
+		self.word = list(word)
+		self.mask = self.CreateWordMask(word)
+
+	def OpenLetters(self, letters: list) -> str:
+		for i in letters:
+			self.mask[i] = self.word[i]
+
+		return self.GetMask()
+
+	def CreateWordMask(self, word):
+		length = len(word)
+		return [self.q for _ in range(length)]
+
+	def GetMask(self):
+		return ''.join(self.mask)
+
+	def GetWord(self):
+		return ''.join(self.word)
+		
+
+@dataclass
+class Player:
+	name: str
+	id: int
+	word: Word
+
+	def __init__(self, id: int, name: str, word: Word):
+		self.name = name
+		self.id = id
+		self.word = word
+
+	def ChangeWord(self, newWord):
+		self.word = newWord
+
 
 class Hangman:
 	def __init__(self):
@@ -28,13 +70,21 @@ class Hangman:
 		self.yesFile = "yes.txt"
 		self.noFile = "no.txt"
 
+		self.invalidGuessReply = """
+We have only English words here.
+
+Please, guess the whole word or just a letter.
+If you want to stop playing, press /stop
+If you want to restart game, press /start
+"""
+
 		self.words = list()
 		self.yesVariants = list()
 		self.noVariants = list()
 
-		self.q = "\U00002753"
-
 		self.LoadFiles()
+
+		self.players = dict()
 
 		@self.bot.message_handler(commands=['start', 'help'])
 		def Welcome(message):
@@ -44,9 +94,20 @@ class Hangman:
 
 			self.bot.register_next_step_handler(reply, self.StartPlaying)
 
+	def AddOrUpdatePlayer(self, message: Message, word: Word):
+		playerId = message.chat.id
+		playerName = message.chat
+		if (playerId in self.players):
+			self.players[playerId].ChangeWord(word)
+		else:
+			self.players[playerId] = Player(playerId, playerName, word)
+
 	def InitializeGame(self, message):
-		self.currentWord = random.choice(self.words)
-		reply = self.bot.send_message(message.chat.id, f"Here is your word: {self.currentWord}. Make a guess!")
+		w = random.choice(self.words)
+
+		self.AddOrUpdatePlayer(message, Word(w))
+
+		reply = self.bot.send_message(message.chat.id, f"Here is your word: {w}. Make a guess!")
 
 		self.bot.register_next_step_handler(reply, self.PlayRound)
 
@@ -67,11 +128,13 @@ class Hangman:
 	def PlayRound(self, message):
 		reply = self.HandleGuess(message)
 
-		if (isinstance(reply, telebot.types.Message)):
+		if (isinstance(reply, Message)):
 			self.bot.register_next_step_handler(reply, self.PlayRound)
 
 	def HandleGuess(self, message):
 		guess = message.text.lower()
+		playerId = message.chat.id
+		word = self.players[playerId].word.GetWord()
 
 		if (guess == "/stop"):
 			self.StopGame(message)
@@ -83,10 +146,10 @@ class Hangman:
 		guessType = self.DetermineGuessType(guess)
 
 		if (guessType == GuessType.Error):
-			return self.bot.send_message(message.chat.id, "We have only English words here. Please, guess the whole word or just a letter.")
+			return self.bot.send_message(message.chat.id, self.invalidGuessReply)
 
 		if (guessType == GuessType.Letter):
-			result = self.CheckLetter(guess, self.currentWord)
+			result = self.CheckLetter(guess, word)
 
 			if (result):
 				reply = self.bot.send_message(message.chat.id, f"Letter guessed correctly at positions {result}!")
@@ -94,7 +157,7 @@ class Hangman:
 				reply = self.bot.send_message(message.chat.id, f"Letter is not found! Wanna try again?")
 
 		elif (guessType == GuessType.Word):
-			result = self.CheckWord(guess, self.currentWord)
+			result = self.CheckWord(guess, word)
 
 			if (result):
 				reply = self.bot.send_message(message.chat.id, f"Word guessed correctly!")
